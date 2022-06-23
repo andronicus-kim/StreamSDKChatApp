9daf1238437caa0692cdc4ef9f87e960b0243562
andronicus_kim <androkim.kim7@gmail.com>
2022-06-23 17:20:25 +0300

    initializes dagger hilt and stream sdk

--- app/src/main/java/com/example/streamchatapp/StreamChatApp.kt
+++ app/src/main/java/com/example/streamchatapp/StreamChatApp.kt
@@ -1,9 +1,22 @@
 package com.example.streamchatapp
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.livedata.ChatDomain
+import javax.inject.Inject
 
 /**
  * Created by Andronicus Kim on 6/23/22
  */
+@HiltAndroidApp
 class StreamChatApp: Application() {
+
+    @Inject
+    lateinit var client: ChatClient
+
+    override fun onCreate() {
+        super.onCreate()
+        ChatDomain.Builder(client,applicationContext).build()
+    }
 }
