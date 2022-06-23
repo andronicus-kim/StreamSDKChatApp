757bca825413cb5bc7e71e2d1b2aa7ef3cd19814
andronicus_kim <androkim.kim7@gmail.com>
2022-06-23 19:35:20 +0300

    handles UI state and subscribes to events

--- app/src/main/java/com/example/streamchatapp/ui/LoginFragment.kt
+++ app/src/main/java/com/example/streamchatapp/ui/LoginFragment.kt
@@ -1,9 +1,19 @@
 package com.example.streamchatapp.ui
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
+import com.example.streamchatapp.R
 import com.example.streamchatapp.databinding.FragmentLoginBinding
+import com.example.streamchatapp.domain.AuthEvent
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 /**
  * Created by Andronicus Kim on 6/23/22
@@ -11,6 +21,52 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class LoginFragment: BaseFragment<FragmentLoginBinding>() {
 
+    private val viewModel: AuthViewModel by viewModels()
+
     override val fragmentBindingInflater: (LayoutInflater) -> ViewBinding
         get() = FragmentLoginBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSubmit.setOnClickListener {
+            setUIState(true)
+            viewModel.signInUser(binding.etUsername.text.toString().trim())
+        }
+
+        binding.etUsername.addTextChangedListener {
+            // clear error when text changes
+            binding.etUsername.error = null
+        }
+
+        subscribeToEvents()
+    }
+
+    private fun setUIState(isLoading: Boolean) {
+       if (isLoading) {
+           binding.progressBar.isVisible = true
+           binding.btnSubmit.isEnabled = false
+       }else {
+           binding.progressBar.isVisible = false
+           binding.btnSubmit.isEnabled = true
+       }
+    }
+
+    private fun subscribeToEvents(){
+        lifecycleScope.launchWhenStarted {
+            viewModel.authEvent.collect { event ->
+                setUIState(false)
+                when(event) {
+                    is AuthEvent.Success -> {
+
+                    }
+                    is AuthEvent.ApiError -> {
+                        Toast.makeText(requireContext(),event.error,Toast.LENGTH_LONG).show()
+                    }
+                    AuthEvent.UserError -> {
+                        binding.etUsername.error = getString(R.string.invalid_username)
+                    }
+                }
+            }
+        }
+    }
 }
