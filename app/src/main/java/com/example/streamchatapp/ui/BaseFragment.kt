2d84424e916ec25b2ae96bfb68b97410a352982d
andronicus_kim <androkim.kim7@gmail.com>
2022-06-23 16:31:00 +0300

    implements abstract base fragment viewbinding class

---
+++ app/src/main/java/com/example/streamchatapp/ui/BaseFragment.kt
@@ -0,0 +1,38 @@
+package com.example.streamchatapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+/**
+ * Created by Andronicus Kim on 6/23/22
+ */
+
+/*
+* Abstract class to be implemented by child fragments for
+* easier viewbinding management
+* */
+abstract class BaseFragment<out T: ViewBinding> : Fragment() {
+
+    protected abstract val fragmentBindingInflater: (LayoutInflater) -> ViewBinding
+
+    private var _binding: ViewBinding? = null
+    @Suppress("UNCHECKED_CAST")
+    protected val binding: T = _binding as T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return fragmentBindingInflater(inflater).root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
