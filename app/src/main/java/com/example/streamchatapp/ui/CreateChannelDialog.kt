2457bb7d200b2fc2b50bed4bef187a530e5b7298
andronicus_kim <androkim.kim7@gmail.com>
2022-06-24 22:01:53 +0300

    adds create channel dialog

---
+++ app/src/main/java/com/example/streamchatapp/ui/CreateChannelDialog.kt
@@ -0,0 +1,9 @@
+package com.example.streamchatapp.ui
+
+import androidx.fragment.app.DialogFragment
+
+/**
+ * Created by Andronicus Kim on 6/24/22
+ */
+class CreateChannelDialog: DialogFragment() {
+}
